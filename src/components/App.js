a1fd06aacbc9b4a459a5e79b23b772b5620008f5
Ocean Abernathy <ocean.abernathy@gmail.com>
2022-11-18 15:34:05 -0700

    Final Push
    Project completed with final functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Router>
           <AuthProvider>
             <Routes>
+              {/* <PrivateRoute exact path='/' component={Dashboard} /> */}
               <Route
                 path='/'
                 element={
@@ -24,8 +27,17 @@ function App() {
                   </PrivateRoute>
                 }
               ></Route>
+              <Route
+                path='/update-profile'
+                element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                }
+              ></Route>
               <Route path='/signup' element={<Signup />} />
               <Route path='/login' element={<Login />} />
+              <Route path='/forgot-password' element={<ForgotPassword />} />
             </Routes>
           </AuthProvider>
         </Router>
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,3 +7,24 @@ export default function PrivateRoute({ children }) {
 
   return currentUser ? children : <Navigate to='/login' />;
 }
+
+// import React from 'react';
+// import { Route, Navigate } from 'react-router-dom';
+// import { useAuth } from '../contexts/AuthContext';
+
+// export default function PrivateRoute({ component: Component, ...rest }) {
+//   const { currentUser } = useAuth();
+
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => {
+//         return currentUser ? (
+//           <Component {...props} />
+//         ) : (
+//           <Navigate to='/login' />
+//         );
+//       }}
+//     ></Route>
+//   );
+// }
